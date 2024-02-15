6622ac607f2e80ecc2b146643dc92c3591c4681c
Tuğberk Urgancı <tugberkurganci@gmail.com>
2024-02-15 11:28:15 +0300

    added

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "https://spring-render-ucd3.onrender.com/",
+
+      "/assets": {
+        target: "https://spring-render-ucd3.onrender.com/",
+        changeOrigin: true,
+        rewrite: (path: any) => {
+          if (path.includes("checkout")) {
+            return path.replace(/^\/checkout/, "");
+          }
+          if (path.includes("car-detail")) {
+            return path.replace(/^\/car-detail/, "");
+          }
+          return path;
+        },
+      },
+    },
+  },
+  
+});
